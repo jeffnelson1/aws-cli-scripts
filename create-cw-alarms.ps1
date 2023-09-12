b0f9e77a06a7cf8cb243afcdbdb081f29b0ca5c3
Jeffrey Nelson <49223599+jeffnelson1@users.noreply.github.com>
2023-09-12 11:08:12 -0500

    Update create-cw-alarms.ps1

--- create-cw-alarms.ps1
+++ create-cw-alarms.ps1
@@ -4,6 +4,6 @@ $instances = $ec2instances.Reservations.Instances
 foreach ($instance in $instances) {
 
     Write-output("Creating alarm for $($instance.InstanceId)")
-    aws cloudwatch put-metric-alarm --alarm-name cpu-mon-$($instance.InstanceId) --alarm-description "Alarm when CPU is greater than or equal to 80%" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 80 --comparison-operator GreaterThanOrEqualToThreshold --dimensions  Name=InstanceId,Value=$($instance.InstanceId) --evaluation-periods 2 --alarm-actions arn:aws:sns:us-west-2:224321149435:CPUutilizationTopic --unit Percent --region us-west-1
+    aws cloudwatch put-metric-alarm --alarm-name cpu-mon-$($instance.InstanceId) --alarm-description "Alarm when CPU is greater than or equal to 80%" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 80 --comparison-operator GreaterThanOrEqualToThreshold --dimensions  Name=InstanceId,Value=$($instance.InstanceId) --evaluation-periods 2 --alarm-actions arn:aws:sns:us-west-2:11111111:CPUutilizationTopic --unit Percent --region us-west-1
     
 }
